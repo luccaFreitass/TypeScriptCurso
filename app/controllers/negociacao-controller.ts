e8c92a1c4e1264969e82adec7f89ca4100068111
Lucca Freitas <luccafreitas@Luccas-MacBook-Air.local>
2024-04-11 10:51:19 -0300

    implementando controller

---
+++ app/controllers/negociacao-controller.ts
@@ -0,0 +1,39 @@
+import { Negociacao } from "../models/negociacao.js";
+
+export class NegociacaoController {
+    private inputData: HTMLInputElement;
+    private inputQuantidade: HTMLInputElement;
+    private inputValor: HTMLInputElement;
+
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+
+    adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao)
+        this.limparFormulario();
+    }
+
+    criaNegociacao(): Negociacao {
+        const exp = /-/g;
+        const data = this.inputData.value.replace(exp, '/');  // Substituindo hífens por barras
+
+        // Criando a data no formato "YYYY/MM/DD"
+        const partesData = data.split('/');
+        const date = new Date(parseInt(partesData[0]), parseInt(partesData[1]) - 1, parseInt(partesData[2]))
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+
+    limparFormulario(): void {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+
+    }
+}
